6ca34f1d3b5995adc59fc5c6188efd086e8b371d
Murad Jafarov <muradjj@code.edu.az>
2023-08-21 15:56:57 +0400

    Latest, Oldest sorting added

--- BitNews/Controllers/NewsController.cs
+++ BitNews/Controllers/NewsController.cs
@@ -34,51 +34,6 @@ namespace BitNews.Controllers
             _layoutService = layoutService;
         }
 
-        //public async Task<IActionResult> Index(string[] selectedTags, string sort, int page = 1, int pageSize = 10)
-        //{
-        //    ViewBag.PageSize = pageSize;
-
-        //    var news = await _newsService.GetAllWithIncludesAsync();
-
-        //    int totalItems = news.Count();
-        //    int totalPages = (int)Math.Ceiling(totalItems / (double)pageSize);
-
-        //    var pagedNews = news
-        //        .Skip((page - 1) * pageSize)
-        //        .Take(pageSize)
-        //        .ToList();
-
-        //    IEnumerable<Category> categories = await _categoryService.GetAll();
-        //    List<NewsVM> model = new List<NewsVM>();
-        //    var datas = await _layoutService.GetAllDatas();
-
-        //    foreach (var item in pagedNews)
-        //    {
-        //        model.Add(new NewsVM
-        //        {
-        //            Id = item.Id,
-        //            Title = item.Title,
-        //            Description = item.Description,
-        //            Image = item.Images?.FirstOrDefault()?.Image,
-        //            CategoryName = item.Category.Name,
-        //            ViewCount = item.ViewCount,
-        //            View = datas,
-        //            CreateDate = item.CreateDate.ToString("dd MMMM yyyy"),
-        //            Tag = string.Join(", ", item.NewsTags.Select(tag => tag.Tag.Name)),
-        //            CreatorName = item.CreatorName,
-
-        //        });
-        //    }
-
-        //    if (selectedTags != null && selectedTags.Length > 0)
-        //    {
-        //        model = model.Where(n => selectedTags.Contains(n.CategoryName)).ToList();
-        //    }
-
-        //    var paginatedModel = new Helpers.Paginate<NewsVM>(model, page, totalPages);
-
-        //    return View(paginatedModel);
-        //}
         public async Task<IActionResult> Index(string[] selectedTags, string sort, string searchText = null, int page = 1, int pageSize = 10)
         {
             ViewBag.PageSize = pageSize;
